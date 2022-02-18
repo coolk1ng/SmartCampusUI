31306f265238b057a49dc3637d39f5290f9b21fe
xwgong4 <xwgong4@iflytek.com>
2022-02-18 17:05:00 +0800

    添加按钮图案

--- src/components/user/PersonalInfo.vue
+++ src/components/user/PersonalInfo.vue
@@ -3,7 +3,7 @@
     <el-descriptions class="margin-top" title="个人信息" :column="2" border>
       <template slot="extra">
         <el-tooltip content="操作" effect="dark">
-          <el-button type="primary" size="small" @click="showEditPage"><i class="fa fa-gavel"></i></el-button>
+          <el-button type="primary" size="small" @click="showEditPage"><i class="fa fa-pencil"></i></el-button>
         </el-tooltip>
       </template>
       <el-descriptions-item>
