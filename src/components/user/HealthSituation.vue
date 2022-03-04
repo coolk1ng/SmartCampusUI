b21c49f2dfd135fa79cc96f384d53370b0495492
CodeSniper <codesniperrrrr@gmail.com>
2022-03-04 10:08:27 +0800

    增加健康状况的表单验证

--- src/components/user/HealthSituation.vue
+++ src/components/user/HealthSituation.vue
@@ -45,7 +45,7 @@
         </el-table-column>
         <el-table-column
             prop="temperature"
-            label="体温">
+            label="体温/℃">
         </el-table-column>
         <el-table-column
             prop="isFever"
@@ -98,15 +98,15 @@
       title="申请信息"
       :visible.sync="dialogVisible"
       width="50%">
-    <el-form ref="form" :model="userHealth" size="mini">
+    <el-form ref="form" :rules="rules" :model="userHealth" size="mini">
       <el-row>
         <el-col :span="9" :offset="2">
-          <el-form-item label="姓名:">
+          <el-form-item label="姓名:" prop="name">
             <el-input size="mini" v-model="userHealth.name" disabled></el-input>
           </el-form-item>
         </el-col>
         <el-col :span="9" :offset="2">
-          <el-form-item label="体温:">
+          <el-form-item label="体温/℃:" prop="temperature">
             <el-input size="mini" v-model="userHealth.temperature"></el-input>
           </el-form-item>
         </el-col>
@@ -148,14 +148,14 @@
           </el-form-item>
         </el-col>
         <el-col :span="9" :offset="2">
-          <el-form-item label="当前所在地:">
+          <el-form-item label="当前所在地:" prop="localToday">
             <el-input size="mini" v-model="userHealth.localToday"></el-input>
           </el-form-item>
         </el-col>
       </el-row>
       <el-row>
         <el-col :span="9" :offset="2">
-          <el-form-item label="健康码颜色:">
+          <el-form-item label="健康码颜色:" prop="healthCodeColor">
             <el-select v-model="userHealth.healthCodeColor" size="mini">
               <el-option label="绿色" value="1"></el-option>
               <el-option label="黄色" value="2"></el-option>
@@ -179,21 +179,23 @@
         </el-col>
       </el-row>
       <el-row>
-        <el-form-item>
-          <el-button size="mini" type="success" style="margin-left: 380px" @click="updateUserHealth">保存</el-button>
-        </el-form-item>
+        <el-col :offset="11">
+          <el-form-item>
+            <el-button size="mini" type="success"  @click="updateUserHealth">保存</el-button>
+          </el-form-item>
+        </el-col>
       </el-row>
     </el-form>
   </el-dialog>
 <!--  填报弹出框-->
   <el-dialog
-      title="申请信息"
+      title="填报信息"
       :visible.sync="dialogVisible2"
       width="50%">
-    <el-form ref="form" :model="userHealth" size="mini">
+    <el-form ref="form" :rules="rules" :model="userHealth" size="mini">
       <el-row>
         <el-col :span="9" :offset="2">
-          <el-form-item label="体温:">
+          <el-form-item label="体温/℃:" prop="temperature">
             <el-input size="mini" v-model="userHealth.temperature"></el-input>
           </el-form-item>
         </el-col>
@@ -218,17 +220,17 @@
             <el-switch
                 class="switch"
                 v-model="userHealth.isContactRisk"
-                active-value="1"
-                inactive-value="0"
-                active-text="是"
-                inactive-text="否"
+                active-value="0"
+                inactive-value="1"
+                active-text="否"
+                inactive-text="是"
                 active-color="#13ce66"
                 inactive-color="#ff4949">
             </el-switch>
           </el-form-item>
         </el-col>
         <el-col :span="9" :offset="2">
-          <el-form-item label="当前所在地:">
+          <el-form-item label="当前所在地:" prop="localToday">
             <el-input size="mini" v-model="userHealth.localToday"></el-input>
           </el-form-item>
         </el-col>
@@ -259,9 +261,11 @@
         </el-col>
       </el-row>
       <el-row>
-        <el-form-item>
-          <el-button size="mini" type="success" style="margin-left: 380px" @click="InsertHealthInfo">填报</el-button>
-        </el-form-item>
+        <el-col :offset="11">
+          <el-form-item>
+            <el-button size="mini" type="success" @click="InsertHealthInfo">填报</el-button>
+          </el-form-item>
+        </el-col>
       </el-row>
     </el-form>
   </el-dialog>
@@ -303,7 +307,15 @@ export default {
         total: 0
       },
       dialogVisible: false,
-      dialogVisible2: false
+      dialogVisible2: false,
+      rules:{
+        temperature: [
+          { required: true, message: '请输入体温', trigger: 'blur' }
+        ],
+        localToday: [
+          { required: true, message: '请输入体温', trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
@@ -344,9 +356,16 @@ export default {
     updateUserHealth(){
       postRequest('/userHealth/updateUserHealth',this.userHealth).then(res=>{
         if (res){
-          this.$message.success("编辑成功");
+          this.$notify({
+            title: '成功',
+            message: '健康状况信息编辑成功',
+            type: 'success'
+          });
         }else{
-          this.$message.error("编辑失败");
+          this.$notify.error({
+            title: '错误',
+            message: '编辑失败',
+          });
         }
         this.dialogVisible = false;
         this.initUserHealthList();
